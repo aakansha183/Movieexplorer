e12557599e2477503a2f1f7bb7f872e06362ceb7
Aakansha Tyagi <aakansha.tyagi@binmile.com>
2024-07-09 10:54:20 +0530

    Some changes

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,14 +2,13 @@ import React, { useState } from 'react';
 import MovieList from '../components/MovieList';
 import SearchBar from '../components/SearchBar';
 import useFavorites from '../hooks/useFavorites';
-import { Movie } from '../types/Movie';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 
 const Favorites: React.FC = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const currentUser = useSelector((state: RootState) => state.users.currentUser);
-    const { favorites, addToFavorites, removeFromFavorites } = useFavorites(currentUser);
+    const { favorites, removeFromFavorites } = useFavorites(currentUser);
 
     const handleSearch = (query: string) => {
         setSearchQuery(query);
